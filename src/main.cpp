367c8514d786a6546e04951f32c4a7af3a671acb
Omar Helmy <omar_helmy@Omars-MacBook-Air.local>
2021-03-14 23:26:09 +0100

    Add todo1

--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,26 @@
 
 using namespace std::experimental;
 
+// Construct start_node and end_node as in the model.
+using Coords = struct { 
+    float start_x = 0.1;
+    float start_y = 0.1;
+    float end_x = 0.9;
+    float end_y = 0.9;
+    friend std::istream& operator>>(std::istream &cin,Coords &c);
+};
+std::istream& operator>>(std::istream &cin, Coords &c) {
+    cin >> c.start_x;
+    cin >> c.start_y;
+    cin >> c.end_x;
+    cin >> c.end_y;
+    return cin;
+}
+
 static std::optional<std::vector<std::byte>> ReadFile(const std::string &path)
 {   
+    // read in binary mode: not convert to strings
+    // read in ate mode: auto seek till AtTheEnd, so we later user tell() to get size
     std::ifstream is{path, std::ios::binary | std::ios::ate};
     if( !is )
         return std::nullopt;
@@ -55,12 +73,14 @@ int main(int argc, const char **argv)
     // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
     // user input for these values using std::cin. Pass the user input to the
     // RoutePlanner object below in place of 10, 10, 90, 90.
+    Coords coords;
+    std::cin >> coords;
 
     // Build Model.
     RouteModel model{osm_data};
 
     // Create RoutePlanner object and perform A* search.
-    RoutePlanner route_planner{model, 10, 10, 90, 90};
+    RoutePlanner route_planner{model, coords.start_x, coords.start_y, coords.end_xx, coords.end_y};
     route_planner.AStarSearch();
 
     std::cout << "Distance: " << route_planner.GetDistance() << " meters. \n";
